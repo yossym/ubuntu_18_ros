de2bc9a1e83ff2f813636cd904bac3165b77a6a4
yossym <docomoxyz@gmail.com>
2021-10-30 04:37:13 +0900

    anything

---
+++ ros_install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+#!/usr/bin/bash -ux
+#  vim:fileencoding=utf-8:ff=unix
+
+cd $(dirname $0);
+## undefined Variable check. if undefined exit 
+#set -u
+## Variable print
+#set -x
+# function __clean(){
+# # 
+# }
+#
+# trap __clean INT QUIT TERM HUP
+
+
+
+
+udo systemctl restart chrony
+for i in 6
+do
+    chronyc sources
+    sleep 1
+done
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install -y ros-melodic-desktop-full
+sudo apt install -y python-rosdep
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+
