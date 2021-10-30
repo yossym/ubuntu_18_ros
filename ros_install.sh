7c1525ce6c4256ccbb90cde95a235cf444afb3b1
yossym <docomoxyz@gmail.com>
2021-10-30 10:58:27 +0900

    anything

--- ros_install.sh
+++ ros_install.sh
@@ -18,11 +18,19 @@ cd $(dirname $0);
 
 
 sudo systemctl restart chrony
-for i in 6
-do
+
     chronyc sources
     sleep 1
-done
+    chronyc sources
+    sleep 1
+    chronyc sources
+    sleep 1
+    chronyc sources
+    sleep 1
+    chronyc sources
+    sleep 1
+    chronyc sources
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
