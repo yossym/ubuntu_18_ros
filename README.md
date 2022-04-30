29778e723027ea8f3dcdcb5b30d4769cc6d81a50
yossym <docomoxyz@gmail.com>
2022-05-01 07:07:04 +0900

    anything

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # ubuntu_19_ros
 ubuntu にROSをインストールするスクリプト
 
+-----------------------------
 git add .
 git commit -m "anything"
-git push origin main
+git push origin master
+
+-------------------
+git clone https://github.com/yossym/dotfiles.git
+git pull origin master
 
--- ros_install.sh
+++ ros_install.sh
@@ -14,32 +14,76 @@ cd $(dirname $0);
 #
 # trap __clean INT QUIT TERM HUP
 
-
-
+#####################################
+# time sync 
 
 sudo systemctl restart chrony
 
-    chronyc sources
-    sleep 1
-    chronyc sources
-    sleep 1
-    chronyc sources
-    sleep 1
-    chronyc sources
-    sleep 1
-    chronyc sources
-    sleep 1
-    chronyc sources
+chronyc sources
+sleep 1
+chronyc sources
+sleep 1
+chronyc sources
+sleep 1
+chronyc sources
+sleep 1
+chronyc sources
+sleep 1
+chronyc sources
+
+#############################################
+# Ubuntu install of ROS Melodic
+# http://wiki.ros.org/melodic/Installation/Ubuntu
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt -y install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
 sudo apt update
-sudo apt install -y ros-melodic-desktop-full
-sudo apt install -y python-rosdep
-sudo rosdep init
-rosdep update
+sudo apt -y install ros-melodic-desktop-full
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+source /opt/ros/melodic/setup.bash
+
+sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+sudo apt -y install python-rosdep
+sudo rosdep init
+rosdep update
+
+###############################
+# ROS Tutorials
+# http://wiki.ros.org/ROS/Tutorials
+
+
+# Installing and Configuring Your ROS Environment
+
+printenv | grep ROS
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+
+
+source devel/setup.bash
+
+
+echo $ROS_PACKAGE_PATH
+pause 30
+
+# Navigating the ROS Filesystem
+
+sudo apt-get install ros-${ROS_DISTRO}-ros-tutorials
+
+
+
+
+# echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+# source ~/.bashrc
+# sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
